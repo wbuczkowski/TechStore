18c89492ba72ec4ca5fd904a5630016f7b48ed17
Wladyslaw Buczkowski <wladyslaw.buczkowski@gmail.com>
2018-09-25 20:52:34 +0200

    visibility of detail view elements

--- app/src/main/java/com/nestle/tp/techstore/DetailActivity.java
+++ app/src/main/java/com/nestle/tp/techstore/DetailActivity.java
@@ -10,7 +10,10 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.common.api.CommonStatusCodes;
 
@@ -38,6 +41,7 @@ public class DetailActivity extends AppActivity {
     public static final String OPTION_INVENTORY_WITH_DOCUMENT = "4";
 
     private String mOption;
+    private TextView mTitle;
     private EditText mWorkOrder;
     private EditText mCostCenter;
     private EditText mMaterial;
@@ -59,6 +63,7 @@ public class DetailActivity extends AppActivity {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(fabListener);
         // get edit text controls
+        mTitle = findViewById(R.id.textTitle);
         mWorkOrder = findViewById(R.id.editWorkOrder);
         mCostCenter = findViewById(R.id.editCostCenter);
         mMaterial = findViewById(R.id.editMaterial);
@@ -72,8 +77,8 @@ public class DetailActivity extends AppActivity {
         if (savedInstanceState != null) {
             mWorkOrder.setText(savedInstanceState.getString(EXTRA_WORK_ORDER, ""));
             mCostCenter.setText(savedInstanceState.getString(EXTRA_COST_CENTER, ""));
-            mPlant.setText(savedInstanceState.getString(EXTRA_PLANT, ""));
-            mStorageLocation.setText(savedInstanceState.getString(EXTRA_STORAGE_LOCATION, ""));
+            //mPlant.setText(savedInstanceState.getString(EXTRA_PLANT, ""));
+            //mStorageLocation.setText(savedInstanceState.getString(EXTRA_STORAGE_LOCATION, ""));
             mInventory.setText(savedInstanceState.getString(EXTRA_INVENTORY, ""));
             mVendor.setText(savedInstanceState.getString(EXTRA_VENDOR, ""));
         }
@@ -87,7 +92,7 @@ public class DetailActivity extends AppActivity {
         data = intent.getStringExtra(EXTRA_COST_CENTER);
         if (data != null) if (!data.isEmpty()) mCostCenter.setText(data);
         data = intent.getStringExtra(EXTRA_MATERIAL);
-        if (data != null) if (!data.isEmpty()) mCostCenter.setText(data);
+        if (data != null) if (!data.isEmpty()) mMaterial.setText(data);
         data = intent.getStringExtra(EXTRA_PLANT);
         if (data != null) if (!data.isEmpty()) mPlant.setText(data);
         data = intent.getStringExtra(EXTRA_STORAGE_LOCATION);
@@ -104,11 +109,99 @@ public class DetailActivity extends AppActivity {
             mPlant.setText(sharedPref.getString("pref_default_plant", ""));
         if (mStorageLocation.length() == 0)
             mStorageLocation.setText(sharedPref.getString("pref_default_storage_location", ""));
-        // TODO: initialize fields labels, visibility and clear unused
-        if (mOption.equals(OPTION_GOODS_ISSUE)) {
+        // initialize visibility and edibility
+        ViewGroup viewGroup;
+        switch (mOption) {
+            case OPTION_GOODS_ISSUE:
+                mTitle.setText(R.string.activity_goods_issue);
+                break;
+            case OPTION_GOODS_RETURN:
+                mTitle.setText(R.string.activity_goods_return);
+                break;
+            case OPTION_INVENTORY_WO_DOCUMENT:
+            case OPTION_INVENTORY_WITH_DOCUMENT:
+                mTitle.setText(R.string.activity_inventory);
+        }
+        if (mOption.equals(OPTION_GOODS_ISSUE) || mOption.equals(OPTION_GOODS_RETURN)) {
+            // hide and clear inventory document number
+            viewGroup = findViewById(R.id.layoutInventory);
+            if (viewGroup != null) viewGroup.setVisibility(View.GONE);
+            mInventory.setText("");
             if (mWorkOrder.length() > 0) {
-
+                // show work order and disable, hide and clear cost center
+                viewGroup = findViewById(R.id.layoutWorkOrder);
+                if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
+                mWorkOrder.setEnabled(false);
+                viewGroup = findViewById(R.id.layoutCostCenter);
+                if (viewGroup != null) viewGroup.setVisibility(View.GONE);
+                mCostCenter.setText("");
+            } else if (mCostCenter.length() > 0) {
+                // show cost center and disable, hide and clear work order
+                viewGroup = findViewById(R.id.layoutCostCenter);
+                if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
+                mCostCenter.setEnabled(false);
+                viewGroup = findViewById(R.id.layoutWorkOrder);
+                if (viewGroup != null) viewGroup.setVisibility(View.GONE);
+                mWorkOrder.setText("");
+            } else {
+                // show and enable both
+                viewGroup = findViewById(R.id.layoutCostCenter);
+                if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
+                mWorkOrder.setEnabled(true);
+                viewGroup = findViewById(R.id.layoutWorkOrder);
+                if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
+                mCostCenter.setEnabled(true);
             }
+        }
+        if (mOption.equals(OPTION_INVENTORY_WO_DOCUMENT) || mOption.equals(OPTION_INVENTORY_WITH_DOCUMENT)) {
+            // show inventory document, hide and clear work order and cost center
+            viewGroup = findViewById(R.id.layoutInventory);
+            if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
+            viewGroup = findViewById(R.id.layoutWorkOrder);
+            if (viewGroup != null) viewGroup.setVisibility(View.GONE);
+            mWorkOrder.setText("");
+            viewGroup = findViewById(R.id.layoutCostCenter);
+            if (viewGroup != null) viewGroup.setVisibility(View.GONE);
+            mCostCenter.setText("");
+            if (mInventory.length() > 0) {
+                mInventory.setEnabled(false);
+            } else {
+                mInventory.setEnabled(true);
+            }
+        }
+        if (mPlant.length() > 0) {
+            // plant provided, disable field
+            mPlant.setEnabled(false);
+        } else {
+            // plant not provided, enable field
+            mPlant.setEnabled(true);
+        }
+        if (mStorageLocation.length() > 0) {
+            // storage location provided, disable field
+            mStorageLocation.setEnabled(false);
+        } else {
+            // storage location not provided, enable field
+            mStorageLocation.setEnabled(true);
+        }
+        if (mBin.length() > 0) {
+            // bin provided, show disabled
+            viewGroup = findViewById(R.id.layoutBin);
+            if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
+            mBin.setEnabled(false);
+        } else {
+            // bin not provided, hide
+            viewGroup = findViewById(R.id.layoutBin);
+            if (viewGroup != null) viewGroup.setVisibility(View.GONE);
+        }
+        if (mVendor.length() > 0) {
+            // vendor provided, show
+            viewGroup = findViewById(R.id.layoutVendor);
+            if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
+            mVendor.setEnabled(false);
+        } else {
+            // vendor not provided, hide
+            viewGroup = findViewById(R.id.layoutVendor);
+            if (viewGroup != null) viewGroup.setVisibility(View.GONE);
         }
     }
 
@@ -117,8 +210,8 @@ public class DetailActivity extends AppActivity {
         super.onSaveInstanceState(outState);
         outState.putString(EXTRA_WORK_ORDER, mWorkOrder.getText().toString());
         outState.putString(EXTRA_COST_CENTER, mCostCenter.getText().toString());
-        outState.putString(EXTRA_PLANT, mPlant.getText().toString());
-        outState.putString(EXTRA_STORAGE_LOCATION, mStorageLocation.getText().toString());
+        //outState.putString(EXTRA_PLANT, mPlant.getText().toString());
+        //outState.putString(EXTRA_STORAGE_LOCATION, mStorageLocation.getText().toString());
         outState.putString(EXTRA_INVENTORY, mInventory.getText().toString());
         outState.putString(EXTRA_VENDOR, mVendor.getText().toString());
     }
@@ -168,24 +261,42 @@ public class DetailActivity extends AppActivity {
     public void processBarcode(String data) {
         String[] splitData = data.split(" ");
         boolean unknown = true;
+        ViewGroup viewGroup;
         if (splitData[0].matches("\\d+")) {
             // starts with a digit
             switch (splitData[0].length()) {
                 case 10:
                     // this is a work order
                     unknown = false;
-                    mWorkOrder.setText(splitData[0]);
+                    if (mOption.equals(OPTION_GOODS_ISSUE) || mOption.equals(OPTION_GOODS_RETURN)) {
+                        // ignore work order by inventory
+                        // show work order, hide and clear cost center
+                        viewGroup = findViewById(R.id.layoutWorkOrder);
+                        if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
+                        mWorkOrder.setText(splitData[0]);
+                        mWorkOrder.setEnabled(false);
+                        viewGroup = findViewById(R.id.layoutCostCenter);
+                        if (viewGroup != null) viewGroup.setVisibility(View.GONE);
+                        mCostCenter.setText("");
+                    }
                     break;
                 case 9:
                     // this is a material
                     unknown = false;
                     mMaterial.setText(splitData[0]);
+                    mMaterial.setEnabled(false);
                     if (splitData.length > 1) {
                         mPlant.setText(splitData[1]);
+                        // plant provided, disable field
+                        mPlant.setEnabled(false);
                         if (splitData.length > 2) {
                             mStorageLocation.setText(splitData[2]);
+                            // storage location provided, disable field
+                            mStorageLocation.setEnabled(false);
                             if (splitData.length > 3) {
                                 mBin.setText(splitData[3]);
+                                viewGroup = findViewById(R.id.layoutBin);
+                                if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
                             }
                         }
                     }
@@ -195,12 +306,20 @@ public class DetailActivity extends AppActivity {
             // this is an ERSA or UNBW material
             unknown = false;
             mMaterial.setText(splitData[0].substring(1, 10));
+            mMaterial.setEnabled(false);
             if (splitData.length > 1) {
                 mPlant.setText(splitData[1]);
+                // plant provided, disable field
+                mPlant.setEnabled(false);
                 if (splitData.length > 2) {
                     mStorageLocation.setText(splitData[2]);
+                    // storage location provided, disable field
+                    mStorageLocation.setEnabled(false);
                     if (splitData.length > 3) {
                         mBin.setText(splitData[3]);
+                        mBin.setEnabled(false);
+                        viewGroup = findViewById(R.id.layoutBin);
+                        if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
                     }
                 }
             }
@@ -208,18 +327,32 @@ public class DetailActivity extends AppActivity {
             // this is a vendor consignment material
             unknown = false;
             mMaterial.setText(splitData[0].substring(1, 10));
+            mMaterial.setEnabled(false);
             if (splitData.length > 1) {
                 if (splitData[1].matches("\\d{9}")) {
                     // this is a vendor code
                     mVendor.setText(splitData[1]);
+                    viewGroup = findViewById(R.id.layoutVendor);
+                    if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
+                    mVendor.setEnabled(false);
                 } else {
                     mPlant.setText(splitData[1]);
+                    // plant provided, disable field
+                    mPlant.setEnabled(false);
                     if (splitData.length > 2) {
                         mStorageLocation.setText(splitData[2]);
+                        // storage location provided, disable field
+                        mStorageLocation.setEnabled(false);
                         if (splitData.length > 3) {
                             mVendor.setText(splitData[3]);
+                            viewGroup = findViewById(R.id.layoutVendor);
+                            if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
+                            mVendor.setEnabled(false);
                             if (splitData.length > 4) {
                                 mBin.setText(splitData[4]);
+                                mBin.setEnabled(false);
+                                viewGroup = findViewById(R.id.layoutBin);
+                                if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
                             }
                         }
                     }
@@ -228,15 +361,32 @@ public class DetailActivity extends AppActivity {
         } else if (splitData[0].startsWith("C")) {
             // this is a cost center
             unknown = false;
-            mCostCenter.setText(splitData[0].substring(1));
+            if (mOption.equals(OPTION_GOODS_ISSUE) || mOption.equals(OPTION_GOODS_RETURN)) {
+                // ignore cost center by inventory
+                // show cost center, hide and clear work order
+                viewGroup = findViewById(R.id.layoutCostCenter);
+                if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
+                mCostCenter.setText(splitData[0].substring(1));
+                viewGroup = findViewById(R.id.layoutWorkOrder);
+                if (viewGroup != null) viewGroup.setVisibility(View.GONE);
+                mWorkOrder.setText("");
+            }
         } else if (splitData[0].matches("V\\d{9}")) {
             // this is a vendor
             unknown = false;
+            // display vendor
+            viewGroup = findViewById(R.id.layoutVendor);
+            if (viewGroup != null) viewGroup.setVisibility(View.VISIBLE);
+            mVendor.setEnabled(false);
             mVendor.setText(splitData[0].substring(1));
         } else if (splitData[0].matches("I\\d{9}")) {
             // this is an inventory document
             unknown = false;
-            mInventory.setText(splitData[0].substring(1));
+            if (mOption.equals(OPTION_INVENTORY_WO_DOCUMENT) || mOption.equals(OPTION_INVENTORY_WITH_DOCUMENT)) {
+                // ignore inventory number at goods issue
+                mInventory.setText(splitData[0].substring(1));
+                mInventory.setEnabled(false);
+            }
         }
         if (unknown) Snackbar.make(findViewById(R.id.fab),
                 "Unknown barcode",
