8fdaed97e73d47f0d4ff1667068d3acca4870440
Wladyslaw Buczkowski <wladyslaw.buczkowski@gmail.com>
2019-01-24 19:07:40 +0100

    Fix number of decimals validation

--- TechStoreApp/src/main/java/com/nestle/tp/techstore/DetailActivity.java
+++ TechStoreApp/src/main/java/com/nestle/tp/techstore/DetailActivity.java
@@ -747,13 +747,14 @@ public class DetailActivity extends AppActivity {
             return false;
         } else {
             double q = Double.parseDouble(s.toString());
+
             if (q == 0.0) {
                 mQuantity.setError(getString(R.string.quantity_zero));
                 return false;
-            } else if (Math.round(q * 1000.0) < q * 1000.0) {
+            } else if (Math.floor(q * 1000.0) < q * 1000.0) {
                 mQuantity.setError(getString(R.string.quantity_decimals_length));
                 return false;
-            } else if (Math.round(q / 10000000000.0) > 0) {
+            } else if (Math.floor(q / 10000000000.0) > 0) {
                 mQuantity.setError(getString(R.string.quantity_integers_length));
                 return false;
             } else {
